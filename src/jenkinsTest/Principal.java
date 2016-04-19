0622209a31477bbbc5280579670804a123a5af56
DIEGO RUIZ <diego25061@gmail.com>
2016-04-19 15:57:34 -0500

    se agrego mensaje de hora y más lineas

--- src/jenkinsTest/Principal.java
+++ src/jenkinsTest/Principal.java
@@ -1,8 +1,13 @@
 package jenkinsTest;
+import java.util.Date;
 
 public class Principal {
 
 	public static void main(String[] args){
-		System.out.println(":V");
+		System.out.println("===================================");
+		System.out.println("Hello :v");
+		Date d = new Date();
+		System.out.println("" + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+		System.out.println("===================================");
 	}
 }
